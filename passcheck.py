1766344df3acc6853d32db0ca1d3438d5d66aebd
maciej <mkrywult88@gmail.com>
2022-09-11 10:17:42 +0200

    add counting of leaks

--- passcheck.py
+++ passcheck.py
@@ -8,9 +8,16 @@ def api_response(query_char):
         raise RuntimeError(f'Error: {res.status_code}, check the api and try again')
     return res
 
+def password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        print(h, count)
+
 def pwnd_check(password):
     password = str(password)
     sh1pass = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-    return sh1pass
+    firs5_char, tail = sh1pass[:5], sh1pass[5:]
+    response = api_response(firs5_char)
+    return password_leaks_count(response, tail)
 
 pwnd_check(123)
